1f061fa1cffe7c8dfc33a781866f629677311f86
Femi Ayodeji <femidotayodeji@gmail.com>
2020-12-19 20:01:04 +0100

    Added flying action

--- Bird.lua
+++ Bird.lua
@@ -16,6 +16,10 @@ end
 function Bird:update(dt)
     self.dy = self.dy + GRAVITY * dt
 
+    if love.keyboard.wasPressed('space') then
+        self.dy = -5
+    end
+
     self.y = self.y + self.dy
 end
 
--- main.lua
+++ main.lua
@@ -32,6 +32,8 @@ function love.load()
         fullscreen = false,
         resizable = true
     })
+
+    love.keyboard.keysPressed = {}
 end
 
 function love.resize(w, h)
@@ -39,8 +41,17 @@ function love.resize(w, h)
 end
 
 function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
     if key == 'escape' then
         love.event.quit()
+    end
+end
+
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
     end
 end
 
@@ -49,6 +60,8 @@ function love.update(dt)
     groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 
     bird:update(dt)
+
+    love.keyboard.keysPressed = {}
 end
 
 function love.draw()
