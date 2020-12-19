57d73751a87d5f4cfa810c348ee71cb6713952c6
Femi Ayodeji <femidotayodeji@gmail.com>
2020-12-19 19:47:53 +0100

    Added gravity

--- Bird.lua
+++ Bird.lua
@@ -1,5 +1,7 @@
 Bird = Class{}
 
+local GRAVITY = 20
+
 function Bird:init()
     self.image = love.graphics.newImage('assets/bird.png')
     self.width = self.image:getWidth()
@@ -7,6 +9,14 @@ function Bird:init()
 
     self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
     self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2) 
+
+    self.dy = 0
+end
+
+function Bird:update(dt)
+    self.dy = self.dy + GRAVITY * dt
+
+    self.y = self.y + self.dy
 end
 
 function Bird:render()
--- main.lua
+++ main.lua
@@ -45,9 +45,10 @@ function love.keypressed(key)
 end
 
 function love.update(dt)
-    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
-    
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT    
     groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+
+    bird:update(dt)
 end
 
 function love.draw()
