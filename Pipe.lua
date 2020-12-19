2e66b6eca0831a26b249d7ffa2da5f8997638e98
Femi Ayodeji <femidotayodeji@gmail.com>
2020-12-19 23:06:30 +0100

    Added pairs of pipe

--- Pipe.lua
+++ Pipe.lua
@@ -1,22 +1,29 @@
 Pipe = Class{}
 
 local PIPE_IMAGE = love.graphics.newImage('assets/pipe.png')
+PIPE_SPEED = 60
+PIPE_HEIGHT = 288
+PIPE_WIDTH = 70
 
-local PIPE_SCROLL = -60
-
-function Pipe:init()
+function Pipe:init(orientation, y)
     self.x = VIRTUAL_WIDTH
-
-    self.y = math.random(VIRTUAL_HEIGHT / 4, VIRTUAL_HEIGHT - 10)
+    self.y = y
 
     self.width = PIPE_IMAGE:getWidth()
+    self.height = PIPE_HEIGHT
+
+    self.orientation = orientation
 end
 
 function Pipe:update(dt)
-    self.x = self.x + PIPE_SCROLL * dt
+
 end
 
 function Pipe:render()
-    love.graphics.draw(PIPE_IMAGE, math.floor(self.x + 0.5), math.floor(self.y))
+    love.graphics.draw(PIPE_IMAGE, self.x,
+        (self.orientation == 'top' and self.y + PIPE_HEIGHT or self.y),
+        0, -- rotation
+        1,
+        self.orientation == 'top' and -1 or 1)
 end
 
