cc72904b985a7d7294579519c42edb2437b1b56e
Femi Ayodeji <femidotayodeji@gmail.com>
2020-12-20 00:00:48 +0100

    Added collision

--- Bird.lua
+++ Bird.lua
@@ -13,6 +13,15 @@ function Bird:init()
     self.dy = 0
 end
 
+function Bird:collides(pipe)
+    if (self.x + 2) + (self.width - 4) >= pipe.x and self.x + 2 then
+        if (self.y + 2) + (self.height - 4) >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then 
+            return true
+        end
+    end
+    return false
+end
+
 function Bird:update(dt)
     self.dy = self.dy + GRAVITY * dt
 
--- main.lua
+++ main.lua
@@ -29,6 +29,7 @@ local pipePairs = {}
 
 local spawnTimer = 0
 local lastY = -PIPE_HEIGHT + math.random(80) + 20
+local scrolling = true
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -63,32 +64,41 @@ function love.keyboard.wasPressed(key)
 end
 
 function love.update(dt)
-    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT    
-    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+    if scrolling then
+        backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT    
+        groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 
-    spawnTimer = spawnTimer + dt
+        spawnTimer = spawnTimer + dt
 
-    if spawnTimer > 2 then
-        local y = math.max(-PIPE_HEIGHT + 10,
-            math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
-        lastY = y
+        if spawnTimer > 2 then
+            local y = math.max(-PIPE_HEIGHT + 10,
+                math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+            lastY = y
 
-        table.insert(pipePairs, PipePair(y))
-        spawnTimer = 0
-    end
-
-    bird:update(dt)
-
-    for k, pair in pairs(pipePairs) do
-        pair:update(dt)
-    end
-
-    for k, pair in pairs(pipePairs) do
-        if pair.remove then 
-            table.remove(pipePairs, k)
+            table.insert(pipePairs, PipePair(y))
+            spawnTimer = 0
         end
-    end
 
+        bird:update(dt)
+
+        for k, pair in pairs(pipePairs) do
+            pair:update(dt)
+        end
+
+        for k, pair in pairs(pipePairs) do
+            for l, pipe in pairs(pair.pipes) do
+                if bird:collides(pipe) then
+                    scrolling = false
+                end
+            end
+
+            if pair.remove then 
+                table.remove(pipePairs, k)
+            end
+        end
+
+    end
+    
     love.keyboard.keysPressed = {}
 end
 
