61c1a36d768fa0af98cf9e854e68300438fb8a28
Femi Ayodeji <femidotayodeji@gmail.com>
2020-12-20 14:37:35 +0100

    Added countdown state

--- main.lua
+++ main.lua
@@ -10,6 +10,7 @@ require 'states/BaseState'
 require 'states/PlayState'
 require 'states/TitleScreenState'
 require 'states/ScoreState'
+require 'states/CountdownState'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -54,6 +55,7 @@ function love.load()
 
     gStateMachine = StateMachine {
         ['title'] = function() return TitleScreenState() end,
+        ['countdown'] = function() return CountdownState() end,
         ['play'] = function() return PlayState() end,
         ['score'] = function() return ScoreState() end,
     }
